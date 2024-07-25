f64ca57caa4742998046a8b11d75ac38a7fffaa6
Nusrot Jahan <101088067+njNus@users.noreply.github.com>
2024-07-25 17:43:13 +0600

    Add files via upload

---
+++ games/Runner.java
@@ -0,0 +1,11 @@
+package Farahpu;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TicTacToe tictactoe = new TicTacToe();
+
+	}
+
+}
---
+++ games/SnakeGame.java
@@ -0,0 +1,11 @@
+package Farahpu;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GameFrame();
+
+	}
+
+}
